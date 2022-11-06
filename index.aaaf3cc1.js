4c440b47a9696743ccbcaf5c04563ce8c8c4c767
Juliia1109 <111360052+Juliia1109@users.noreply.github.com>
2022-11-06 22:43:53 +0000

    Deploying to gh-pages from @ Juliia1109/goit-js-hw-10@e4b789e0b25249180777275520474272d7ab100f 🚀

---
+++ index.aaaf3cc1.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/").then((function(n){return n.json()})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}));
+//# sourceMappingURL=index.aaaf3cc1.js.map
---
+++ index.dfd96526.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/").then((o=>o.json())).then((o=>{console.log(o)})).catch((o=>{console.log(o)}));
+//# sourceMappingURL=index.dfd96526.js.map
