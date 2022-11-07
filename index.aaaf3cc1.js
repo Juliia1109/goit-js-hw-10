2015fcbbfacc60d361b3bca04d64e717494d4f25
Juliia1109 <111360052+Juliia1109@users.noreply.github.com>
2022-11-07 01:37:54 +0000

    Deploying to gh-pages from @ Juliia1109/goit-js-hw-10@56430cc0f1a7abfb0cc7d8f0e50dd30f26e5b7d0 🚀

--- index.aaaf3cc1.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/").then((function(n){return n.json()})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}));
-//# sourceMappingURL=index.aaaf3cc1.js.map
